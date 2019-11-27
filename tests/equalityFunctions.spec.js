3e24d938ae360b48fdf05da2801463e2d446dcbe
Andres Kovalev <mail@akovalev.ru>
2019-11-27 22:21:56 +0100

    test: added unit tests for shallowEqual() interacting with arrays

--- tests/equalityFunctions.spec.js
+++ tests/equalityFunctions.spec.js
@@ -13,14 +13,35 @@ describe('strictEqual', () => {
 });
 
 describe('shallowEqual', () => {
-    it('should return true when both arguments have equal fields', () => {
-        const field1 = {};
-        const field2 = {};
+    it('should return true for equal objects', () => {
+        const value = {};
 
-        expect(shallowEqual({ field1, field2 }, { field1, field2 })).toBe(true);
+        expect(shallowEqual(value, value)).toBe(true);
     });
 
-    it('should return false when arguments have not equal fields', () => {
-        expect(shallowEqual({ field1: {} }, { field1: {} })).toBe(false);
+    describe('for objects', () => {
+        it('should return true when both arguments have equal fields', () => {
+            const field1 = {};
+            const field2 = {};
+
+            expect(shallowEqual({ field1, field2 }, { field1, field2 })).toBe(true);
+        });
+
+        it('should return false when arguments have not equal fields', () => {
+            expect(shallowEqual({ field1: {} }, { field1: {} })).toBe(false);
+        });
+    });
+
+    describe('for arrays', () => {
+        it('should return true when both arguments have equal fields', () => {
+            const item1 = {};
+            const item2 = {};
+
+            expect(shallowEqual([ item1, item2 ], [ item1, item2 ])).toBe(true);
+        });
+
+        it('should return false when arguments have not equal fields', () => {
+            expect(shallowEqual([ {} ], [ {} ])).toBe(false);
+        });
     });
 });
