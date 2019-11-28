3d176c48b19b3972420241d7e27484b71f9dcbf5
Andres Kovalev <mail@akovalev.ru>
2019-11-29 00:21:36 +0100

    fix: improved shallowEqual() to work with falsy values

--- src/equalityFunctions.js
+++ src/equalityFunctions.js
@@ -1,9 +1,24 @@
 const strictEqual = (value1, value2) => value1 === value2;
 
-const shallowEqual = (object1, object2) => Object.entries(object1)
-    .every(
+const shallowEqual = (object1, object2) => {
+    if (object1 === object2) {
+        return true;
+    }
+
+    if (!object1 || !object2) {
+        return false;
+    }
+
+    const entries = Object.entries(object1);
+
+    if (entries.length !== Object.entries(object2).length) {
+        return false;
+    }
+
+    return entries.every(
         ([ key, value ]) => value === object2[key]
     );
+};
 
 module.exports = {
     strictEqual,
